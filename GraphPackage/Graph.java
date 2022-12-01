4a3903f841a0a39da750e3494a6f03df12a62b4a
Nicholas Amancio <nick.amancio44@gmail.com>
2022-12-01 01:56:29 -0800

    added the breadth first search algorithm

--- GraphPackage/Graph.java
+++ GraphPackage/Graph.java
@@ -5,7 +5,29 @@ import java.util.Queue;
 public class Graph<T> implements GraphAlgorithmsInterface<T>, BasicGraphInterface<T>{
 
     public QueueInterface<T> getBreadthFirstTraversal(T origin) {
-        
+        resetVertices();
+	    QueueInterface<T> traversalOrder = new LinkedQueue();
+	    QueueInterface<VertexInterface<T>> vertex = new LinkedQueue<VertexInterface<T>>();
+
+	    VertexInterface<T> originVertex = vertices.getValue(origin);
+	    originVertex.visit();
+	    traversalOrder.enqueue(originVertex);
+	
+	    while (!vertexQueue.isEmpty()) {
+		    VertexInterface<T> frontVertex = vertexQueue.dequeue();
+
+		    Iterator<VertexInterface<T>> neighbors = frontVertex.getNeighborsIterator();
+
+		    while (neighbors.hasNext) {
+			    VertexInterface<T> nextNeighbor = newNeighbors.next();
+			    if  (!nextNeighbor.isVisited()) {
+				    nextNeighbor.visit();
+				    traversalOrder.enqueue(nextNeighbor.getLabel());
+				    vertexQueue.enqueue(nextNeighbor);
+			    }  //end if
+		    } //end while
+	    }  //end while
+	    return traversalOrder;
     }
     public QueueInterface<T> getDepthFirstTraversal(T origin) {
 
@@ -41,6 +63,6 @@ public class Graph<T> implements GraphAlgorithmsInterface<T>, BasicGraphInterfac
 
     }
     public void clear() {
-        
+
     }
 }
